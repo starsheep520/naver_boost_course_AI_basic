e7839d9a8c5b3af6d33258d6550a54f7d949dedb
tidestides <98093071+tidestides@users.noreply.github.com>
2022-02-14 20:53:40 +0900

    Update 4_Q5.py

--- Week4/Subin/4_Q5.py
+++ Week4/Subin/4_Q5.py
@@ -1,10 +1,28 @@
 import matplotlib.pyplot as plt
 
 
+#5
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 names = ['group_a', 'group_b', 'group_c']
 values = [1, 10, 100]
-plt.figure(figsize=(9,3))
 
-plt.
+#plt.figure: 새로운 figure 생성
+plt.figure(figsize =(9,3))
 
-#강의 다시 보면서 이해 중
+#막대그래프
+plt.bar(names, values,)
+
+#점 그래프
+plt.figure()
+plt.scatter(names, values)
+
+#선 그래프
+plt.figure()
+plt.plot(names, values)
+
+plt.show()
+
+#데이터 라벨이 미표기되는 
