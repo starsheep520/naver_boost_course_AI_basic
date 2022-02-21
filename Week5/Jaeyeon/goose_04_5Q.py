6e9d3485c2bab274c06f3666c2beddcb46fd48e0
2jaey <2jaeyxxx@gmail.com>
2022-02-21 22:00:45 +0900

    0221 edit

--- Week5/Jaeyeon/goose_04_5Q.py
+++ Week5/Jaeyeon/goose_04_5Q.py
@@ -14,10 +14,11 @@ mnist_test = dset.MNIST(root=root,train=False,transform = transforms.ToTensor(),
 train_loader =  DataLoader(mnist_train,batch_size = batch_size,shuffle=True)
 test_loader = DataLoader(mnist_test,batch_size = batch_size,shuffle=True)
 
-device = torch.device('cpu')
-linear = nn.Linear(28*28,10,bias = True).to(device)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+linear = torch.nn.Linear(784,10,bias = True).to(device)
+torch.nn.init.normal_(linear.weight)
 
-criterion = nn.CrossEntropyLoss().to(device)
+criterion = torch.nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.SGD(linear.parameters(),lr=0.1)
 
 for epoch in range(training_epochs):
@@ -36,17 +37,18 @@ for epoch in range(training_epochs):
                 accuracy = (labels == argmax).float().mean()
                 
                 if (i+1)%100 == 0:
-                    print(f'Epoch:[{epoch+1}/{training_epochs}], Step:[{i+1}/{len(train_loader)}], Loss:[{loss.item():.4f}], Accuracy:[{accuracy.item()*100 :.2f}]' )
+                    print('Epoch [{}/{}], Step[{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'.format(
+                          epoch+1, training_epochs, i+1, len(train_loader), loss.item(), accuracy.item()*100))
 
 linear.eval()
 with torch.no_grad():
     correct = 0
     total = 0
     for i ,(imgs, labels) in enumerate(test_loader):
-        imgs,labels =imgs.to(device),labels.to(device)
+        imgs,labels =imgs.to(device), labels.to(device)
         imgs = imgs.view(-1,28*28)
         outputs = linear(imgs)
         _,argmax = torch.max(outputs,1)
         total += imgs.size(0)
         correct += (labels==argmax).sum().item()
-    print(f'Test accuracy for {total} images: {correct/total * 100:.2f}')
+    print('Test accuracy for {} images: {:.2f}%'.format(total, correct / total *100))
